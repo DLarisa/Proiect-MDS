733c7e1b1167af4d41fce101a49e6778030f152d
DLarisa <56414737+DLarisa@users.noreply.github.com>
2024-11-01 04:34:58 +0200

    Final Update

--- Site MERGE - V01/connect.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// DATABASE NAME = proiectmds !!!
-
-$conn = @mysqli_connect('localhost', 'root', 'root', 'proiectmds') or die('Nu s-a putut efectua conexiunea la baza de date. Try again! :(');
-// servername, username, password, databasename
-
-?>
